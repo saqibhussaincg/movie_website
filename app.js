634e5513e42f177deae9ff2025516133a20e4e48
saqibhussaincg <saqibhussaincg@gmail.com>
2022-08-24 21:23:41 +0500

    working

---
+++ app.js
@@ -0,0 +1,27 @@
+const arrows = document.querySelectorAll(".arrow");  
+const movieLists = document.querySelectorAll(".movie-list");
+
+
+arrows.forEach((arrow, index) => {
+    const itemNumber = movieLists[index].querySelectorAll("img").length;
+
+    let clickCounter = 0;
+
+    arrow.addEventListener("click", () => {
+        clickCounter++;
+        if(itemNumber - (4+clickCounter) >= 0){
+
+            movieLists[index].style.transform = `translateX(${movieLists[index].computedStyleMap().get("transform")[0].x.value - 300}px)`;
+        }
+        else{
+            movieLists[index].style.transform = "translateX(0)";
+
+            clickCounter = 0;
+        }
+    });
+
+    
+    // console.log(movieLists[index].computedStyleMap().get("transform")[0].x.value)
+
+    console.log(movieLists[index].querySelectorAll("img").length)
+})
